0385f49fe4bca56bf2fa387605729d958e05bdeb
RPTechio <304980643+M3gaByteMSA1@users.noreply.github.com>
2026-08-31 19:33:51 -0500

    Completed my SW201 using Command structures. Raghav Peesapati

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,33 +1,46 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+
+
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.ArmConstants.OperatorConstants;
 
-public class RobotContainer {
+
+public class RobotContainer extends Command  {
   // The robot's subsystems are defined here
   ArmSubsystem pivot = new ArmSubsystem();
-
+  TeleopCommands teleopCommands = new TeleopCommands(pivot);
+    
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
+  
   public RobotContainer() {
     // Configure the trigger bindings
+    
     configureBindings();
+
+
+
+
   }
 
   private void configureBindings() {
-
     // TODO: move these command definitions into a new file called TeleopCommands.java
+  
+    m_driverController.b()
+        .onTrue(teleopCommands.practiceCommand());
+    m_driverController.x()
+        .onTrue(teleopCommands.parallelCommand());
+  }
 
     // TODO: replace this with a single command using m_driverController.b().whileTrue(...)
-    m_driverController.b().onTrue(Commands.runOnce(() -> pivot.setPosition(0.39))).onFalse(Commands.runOnce(() -> pivot.setPosition(0)));
+   // m_driverController.b().onTrue(Commands.runOnce(() -> pivot.setPosition(0.39))).onFalse(Commands.runOnce(() -> pivot.setPosition(0)));
 
     // TODO: Add a sequential command to button B that sets the pivot to 0.39 rotations, waits 2 seconds, and then sets the pivot back to 0 
 
     // TODO: Add a parallel command to button X that sets the pivot to 0.39 rotations and prints a message to the console at the same time 
-  }
 }
+
--- src/main/java/frc/robot/TeleopCommands.java
+++ src/main/java/frc/robot/TeleopCommands.java
@@ -6,11 +6,53 @@ import frc.robot.subsystems.arm.ArmSubsystem;
 
 public class TeleopCommands
 {
-    ArmSubsystem arm;
+    private ArmSubsystem arm;
+
     public TeleopCommands(ArmSubsystem arm)
     {
         this.arm = arm;
     }
 
-    // TODO: your code here
+    // My Code:
+
+    public Command practiceCommand(){
+        return Commands.sequence(
+            Commands.runOnce(
+                () -> arm.setPosition(0.0) //Replace with Min
+            
+    
+            ),
+        
+
+            Commands.waitSeconds(1.0),
+        
+            Commands.runOnce(
+                () -> arm.setPosition(0.2) //Replace With Midpoint
+            ),
+
+            Commands.waitSeconds(1.0),
+
+            Commands.runOnce(
+                () -> arm.setPosition(0.4)  //Replace With Extreme
+
+            )
+
+        
+     
+        );
+    }
+    public Command parallelCommand(){
+        return Commands.parallel(
+            Commands.runOnce(
+                ()->arm.setPosition(0.2)
+
+            ),
+            Commands.runOnce(
+               () -> System.out.println("Pivot Arm is moving. X button has been activated!")
+
+            )
+
+        );
+    }
+
 }
